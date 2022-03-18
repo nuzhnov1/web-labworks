ecffef683fff111fce9f58103b7cf2074030d343
Andrey <andrey_nuzhnov.01@mail.ru>
2022-03-18 19:30:13 +0700

    Replace deprecated interface of session variables register to new interface

--- Lab3/Graphics/exit.php
+++ Lab3/Graphics/exit.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-session_unregister('username');
-session_unregister('login');
-session_unregister('user_level');
+$_SESSION = array();
 session_destroy();
 header("Location: index.php");
